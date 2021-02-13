df53045f4ed83dbc68a3d133e6ef284475a220db
suman2020 <47877229+suman2020@users.noreply.github.com>
2021-02-12 22:54:19 -0600

    updated dictionary methods and built in functions

--- dictionary.py
+++ dictionary.py
@@ -46,7 +46,8 @@ print(mfd['age'])
 # adding a new element
 mfd['wiffe'] = 'Albina'
 # adding multiple elements
-mfd.update({'focus':'family','children':2,'siblings':'1'})
+newLis = {'focus':'family','children':2,'siblings':'1'}
+mfd.update(newLis)
 print(mfd)
 
 # time complexity of insertion of element = O(1) and space complexity is : amortized O(1)
@@ -111,7 +112,85 @@ print(mfd)
 # using del() method
 del mfd['name']
 print(mfd)
-del mfd #can delete the entire dictionary as well.
-print(mfd)
+# del mfd #can delete the entire dictionary as well.
+# print(mfd)
 
 # time complexity is generally : O(1) but in some cases it might take longer time to delete so we hace time complexity as : amortized O(n) and space complexity O(1)
+
+"""
+Dictionary methods.
+    1. clear() : removes the entire dictionary items ---> dictionary-name.clear()
+    2. copy() : copy the elements of the dictionary ---> dictionary_name.copy()
+    3. fromkeys(keys,value) : returns a dictionary with the specified keys and the specified value (None if value is not given)
+    4. get(index,value): returns the value for the specified key
+    5. items() : return a key-value tuple pair
+    6. keys() : displays the list of all keys in the dictionary
+    7. values() : displays the list of all the valuee
+    8. popitem() : remove an arbitrary key-value pair in the dictionary
+    9. setdefault(key,value): search for a value with the given key, if not found makes a new dictionary key-value pair
+    10. pop(key): removes the element with the given index
+    11. update(): takes dictionary or tuple as a parameter
+ """
+# copy()
+copydict = mfd.copy()
+copydict['name']= 'Simon'
+print(copydict)
+print(mfd)
+
+# fromkeys()
+x = ('key1','key2','key3')
+z = ['keya','keyb','keyc']
+y = 0
+a = [1,2,3]
+newDict1 = dict.fromkeys(x,y)
+newDict2 = dict.fromkeys(z,a)
+print(newDict1)
+print(newDict2)
+
+
+# get()
+  # returns the value associated with the key. if the value is not found it returns null
+val = copydict.get('name')
+print(val)
+print(copydict)
+
+# if the address is not found it returns the value passed alongside it but that key value pair wont be added to the dictionary
+val2 = copydict.get('address')
+print(val2)  #---> None
+print(copydict)
+
+val3 = copydict.get('address','America')
+print(val3) #---> America
+print(copydict)
+
+
+# setdefault() looks out for the value with the specified key, if key is not present it adds that key along with the passed value as a dictionary item
+val3 =  copydict.setdefault('name')
+print(val3) #--> 'Simon'
+val4 = copydict.setdefault('address','America')
+print(val4) #-->'America'
+print(copydict)
+
+"""
+Dictionary operatons/ built in functions
+1. in operator : returns boolean 
+2. for operator: traverse through the dictionary
+3. all() : returns true if all the values of the key-value pair is true and vice versa , returns true if the dict is empty
+4. any() : returns true if either one value in the key-value pair is true, retunrs false if the dict in empty
+5. len() : returns the number of pairs
+6. sorted() : returns list of keys ,sorts on the basis of ascendig order unless reverse = true (sorts on descendig order)
+"""
+
+result = 'name' in copydict
+result2 ='Simon' in copydict.values()  # ---> time complexity is O(1)
+print(result)
+print(result2)
+
+dicta = {0:False,1:True,2:False}
+print(all(dicta))
+print(any(dicta))
+
+print(len(dicta))
+print(sorted(copydict))
+print(sorted(copydict, reverse= True))
+print(sorted(copydict,key = len))
