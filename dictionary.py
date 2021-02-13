d0eb6da4bb57cb9326bb1988842acc72f3041df2
suman2020 <47877229+suman2020@users.noreply.github.com>
2021-02-12 21:33:28 -0600

    traversing, inserting and searching element in a dictionary

--- dictionary.py
+++ dictionary.py
@@ -1,8 +1,8 @@
 # Dictionary
 # it is a collection which is unordered, changeable and indexed
 
-# creating a dictionary
 
+"""Creation of Dictionary in python"""
 myDict = dict()
 secondDict = { }
 
@@ -37,3 +37,54 @@ Dictionary in memory:
     
 
 """
+"""
+Inserting and updating an element in a Dictionary
+"""
+mfd = {'name':'Suman','age':34}
+mfd['age'] = 38
+print(mfd['age'])
+# adding a new element
+mfd['wiffe'] = 'Albina'
+print(mfd)
+
+# time complexity of insertion of element = O(1) and space complexity is : amortized O(1)
+
+"""
+Traversing through a dictionary:
+"""
+def traverseDict(mfd):
+    # traversing through keys
+    for key in mfd: # can also be written as mfd.keys()
+        print(key)
+    # traversing through values
+    for values in mfd.values():
+        print(values)
+    # traversing through pairs
+
+    for key, value in mfd.items():
+        print(key, value)
+        """
+        can also be written as:
+        for pairs in mfd.items():
+        print(pairs[0],pairs[1])
+        """
+
+traverseDict(mfd)
+
+# time complexity is: O(n)  and space complexity is: 0(1) cause we are not adding or using any space in the memory.
+"""
+Searching for an element in the dictionary
+"""
+def searchDict(dict, value):
+    for key in dict.keys():
+        if dict[key]==value:
+            return key,value  # returns tuple
+            # return [key, value] ---> returns list
+            # return key      --> return str
+
+    return "Value not found"
+
+suman = searchDict(mfd,'Albina')
+print(type(suman))
+# if the value is same for two different keys, then while retruning its gonna return the first key
+# time complexity is: O(n) and space complexity is: O(1)
